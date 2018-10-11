bbcd4f3dad3dbbafe8b2db39e03c84432e91f6f0
Priyanka Phapale <pbphapale@gmail.com>
2018-10-11 11:00:56 -0400

    Updating Notes

--- src/Note.js
+++ src/Note.js
@@ -26,16 +26,20 @@ class Note extends Component {
         alert('removing note');
     }
 
-    save() {
-        alert(this._newText.value);
+    save(e) {
+        e.preventDefault();
+        this.props.onChange(this._newText.value, this.props.index);
+        this.setState({
+            editing: false
+        })
     }
 
     renderForm() {
         return (
             <div className="note">
-                <form>
+                <form onSubmit={this.save}>
                     <textarea ref={input => this._newText = input} />
-                    <button onClick={this.save}><FaSave /></button>
+                    <button id="save"><FaSave /></button>
                 </form>
             </div>
         )
