db3144acfa94b4912483978c8b283e0f96c24566
Priyanka Phapale <pbphapale@gmail.com>
2018-10-11 12:45:11 -0400

    Updates

---
+++ precache-manifest.14c3aeb808ebb51511fce38e90348916.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18a1111bdec1c80d62b",
+    "url": "/Bulletin-Board/static/js/runtime~main.e18a1111.js"
+  },
+  {
+    "revision": "c6a8a9d371114be760df",
+    "url": "/Bulletin-Board/static/js/main.c6a8a9d3.chunk.js"
+  },
+  {
+    "revision": "9f3a44820c8a0d95200f",
+    "url": "/Bulletin-Board/static/js/1.9f3a4482.chunk.js"
+  },
+  {
+    "revision": "c6a8a9d371114be760df",
+    "url": "/Bulletin-Board/static/css/main.53b88c93.chunk.css"
+  },
+  {
+    "revision": "03499e4733444ba5cdeea171899c7c57",
+    "url": "/Bulletin-Board/index.html"
+  }
+];
